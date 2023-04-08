c00055e1e00d86601bac9ac08a77735d48d4957d
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-04-08 17:34:14 +0800

    Update arqjt.js

--- arqjt.js
+++ arqjt.js
@@ -36,11 +36,11 @@
 
 [rewrite_local]
 
-^https?:\/\/.*\.aoscdn\.com url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/arqjt.js
+^https?:\/\/.*\.aoscdn\.(com|cn) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/arqjt.js
 
 [mitm] 
 
-hostname = *.aoscdn.com
+hostname = *.aoscdn.com, *.aoscdn.cn
 
 *************************************/
 
